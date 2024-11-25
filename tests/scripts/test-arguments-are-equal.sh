ad1ee7d2eada1aab6501633414cafbda0f283c0c
antublue <antublue@antublue.org>
2024-11-24 23:23:41 -0500

    Changed test scripts to only show basename
    Signed-off-by: antublue <antublue@antublue.org>

--- tests/scripts/test-arguments-are-equal.sh
+++ tests/scripts/test-arguments-are-equal.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "[FAIL] $0 | argument count [$#] != [2]"
+    echo "[FAIL] $basename | argument count [$#] != [2]"
     exit 1
 fi
 
+basename=$(basename $0)
 arg1=$1
 arg2=$2
 
-echo "[TEST] $0 | [$arg1] == [$arg2]"
+echo "[TEST] $basename | [$arg1] == [$arg2]"
 
 if [ "$arg1" == "$arg2" ]; then
-    echo "[PASS] $0 | [$arg1] == [$arg2]"
+    echo "[PASS] $basename | [$arg1] == [$arg2]"
     exit 0
 else
-    echo "[FAIL] $0 | [$arg1] != [$arg2]"
+    echo "[FAIL] $basename | [$arg1] != [$arg2]"
     exit 1
 fi
