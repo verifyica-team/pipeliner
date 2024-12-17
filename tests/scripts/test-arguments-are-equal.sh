c89a9a9b206d5c09a29c70274c8b0cee6277169c
antublue <antublue@antublue.org>
2024-12-17 00:31:33 -0500

    Updated test scripts
    Signed-off-by: antublue <antublue@antublue.org>

--- tests/scripts/test-arguments-are-equal.sh
+++ tests/scripts/test-arguments-are-equal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "[FAIL] $basename | argument count [$#] != [2]"
+    echo "[FAIL] $basename expected 2 arguments"
     exit 1
 fi
 
@@ -9,12 +9,12 @@ basename=$(basename "$0")
 arg1=$1
 arg2=$2
 
-echo "[TEST] $basename | [$arg1] == [$arg2]"
+echo "[TEST] $basename [$arg1] == [$arg2]"
 
 if [ "$arg1" == "$arg2" ]; then
-    echo "[PASS] $basename | [$arg1] == [$arg2]"
+    echo "[PASS] $basename [$arg1] == [$arg2]"
     exit 0
 else
-    echo "[FAIL] $basename | [$arg1] != [$arg2]"
+    echo "[FAIL] $basename [$arg1] != [$arg2]"
     exit 1
 fi
